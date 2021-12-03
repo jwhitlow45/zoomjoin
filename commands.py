acc6a0773a1b840d2b0e842b8cf1c529576300dc
jwhitlow45 <jwhitlow45@gmail.com>
2021-12-03 18:05:59 -0500

    feat:add method to list commands

---
+++ commands.py
@@ -0,0 +1,15 @@
+def add(name: str, link: str, passwd: str = ''):
+    pass
+
+def remove(meeting_name: str):
+    pass
+
+def join(meeting_name: str):
+    pass
+
+def list_classes():
+    pass
+
+def help():
+    commands = ['add','remove','join','ls']
+    print(f'Available commands are: {commands}')
