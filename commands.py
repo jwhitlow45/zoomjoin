78825e0921274692606ace586920a3b106ae63b5
jwhitlow45 <jwhitlow45@gmail.com>
2021-12-03 18:11:26 -0500

    feat:add command to list available meetings

--- commands.py
+++ commands.py
@@ -1,3 +1,5 @@
+import file_handler as fh
+
 def add(name: str, link: str, passwd: str = ''):
     pass
 
@@ -7,8 +9,12 @@ def remove(meeting_name: str):
 def join(meeting_name: str):
     pass
 
-def list_classes():
-    pass
+def list_meetings():
+    try:
+        meetings = fh.read_meetings_file()
+    except Exception as e:
+        print(e)
+    print(f'Available meetings are: {[key for key in meetings]}')
 
 def help():
     commands = ['add','remove','join','ls']
